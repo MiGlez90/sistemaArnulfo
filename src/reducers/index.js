5fe719335d3a36ecbfcaa48a81d256dd43f7ee11
Miguel Gonzalez <miglez90290995@gmail.com>
2017-10-03 13:13:50 -0500

    Agregacion de usuario a root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux';
 import ingresoReducer from './ingresoReducer';
 import tipoReducer from './tipoReducer';
 import gastoReducer from "./gastoReducer";
+import usuarioReducer from "./usuarioReducer";
 
 
 const rootReducer = combineReducers({
     ingresos: ingresoReducer,
     tipos: tipoReducer,
-    gastos:gastoReducer
+    gastos:gastoReducer,
+    usuario: usuarioReducer
 });
 
 export default rootReducer;
